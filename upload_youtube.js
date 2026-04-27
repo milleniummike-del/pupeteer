ef1db547e1e9029839505adaf1819d518806e6cf
unknown <you@example.com>
2026-04-27 10:08:31 +0200

    update

--- upload_youtube.js
+++ upload_youtube.js
@@ -2,7 +2,9 @@
  * YouTube Upload Automation (File-based metadata)
  */
 
-const channel = 'https://studio.youtube.com/channel/UCotGGoP_MQUh6lgB1smxrfw';
+const channels = ['https://studio.youtube.com/channel/UCwUI5e_vV229JZZcTLoIdgg','https://studio.youtube.com/channel/UCotGGoP_MQUh6lgB1smxrfw', 'https://studio.youtube.com/channel/UC5A2FeUQSnut7JqHRNGGmBA']; // drone, creation, animals
+
+const channel = channels[2];
 
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
